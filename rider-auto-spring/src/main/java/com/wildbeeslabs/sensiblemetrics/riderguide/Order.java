f06364692f5084c9e0dc96af3fce7e73be8b2161
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-04-28 16:38:27 +0300

    Changeset to orders

--- rider-auto-spring/src/main/java/com/wildbeeslabs/sensiblemetrics/riderguide/Order.java
+++ rider-auto-spring/src/main/java/com/wildbeeslabs/sensiblemetrics/riderguide/Order.java
@@ -7,46 +7,36 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
 import org.apache.camel.dataformat.bindy.annotation.DataField;
+import org.apache.commons.lang.StringUtils;
 
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = false)
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @CsvRecord(separator = ",", skipFirstLine = true)
 public class Order implements Serializable {
+
     @XmlAttribute
     @DataField(pos = 1)
     private String name;
-    
+
     @XmlAttribute
     @DataField(pos = 2)
     private int amount;
-    
+
     public Order() {
+        this(StringUtils.EMPTY, 0);
     }
-    
-    public Order(String name, int amount) {
+
+    public Order(final String name, int amount) {
         this.name = name;
         this.amount = amount;
-    }
-    
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (other == null) {
-            return false;
-        }
-        if (this.getClass() != other.getClass()) {
-            return false;
-        }
-        Order that = (Order) other;
-        return this.name.equals(that.name) && this.amount == that.amount;
-    }
-    
-    @Override
-    public String toString() {
-        return "Order[" + name + " , " + amount + "]";
     }
 }
--- rider-auto-spring/src/test/java/com/wildbeeslabs/sensiblemetrics/riderguide/OrderRouterExtension.java
+++ rider-auto-spring/src/test/java/com/wildbeeslabs/sensiblemetrics/riderguide/OrderRouterExtension.java
@@ -3,9 +3,9 @@ package com.wildbeeslabs.sensiblemetrics.riderguide;
 import org.apache.camel.builder.RouteBuilder;
 
 public class OrderRouterExtension extends RouteBuilder {
+
     @Override
-    public void configure() throws Exception {        
+    public void configure() throws Exception {
         from("jms:orders").to("mock:orders");
     }
-
 }
